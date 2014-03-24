3d9986f5b24e472b2e26d636da24ab677dba8e11
bmrobin <bmrobin@g.clemson.edu>
2014-03-24 17:04:02 -0400

    renamed matrix solving method, added code to sum a row

--- MatrixSolver/src/bmrobin/MatrixSolver.java
+++ MatrixSolver/src/bmrobin/MatrixSolver.java
@@ -6,20 +6,33 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class MatrixSolver {
-
+	
 	public static void main(String[] args) {
-		loadData("testInput.txt");
+		try {
+			String file = new String(args[0]);
+			solveMatrix(file);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("No input file specified. Exiting...");
+			System.exit(1);
+		}	
 	}
 	
-	
-	private static void loadData(String filename) {
+	private static void solveMatrix(String filename) {
+		
 		BufferedReader buffer = null;
 		String str = null;
+		String[] row = null;
+		String[] column = null;
+		
 		try {
 			buffer = new BufferedReader(new FileReader(filename));
 			str = buffer.readLine();
 			while (str != null) {
-				System.out.println(str);
+				row = str.split("\\s");
+				sum(row);
+				
+				
+				
 				str = buffer.readLine();
 			}
 			
@@ -30,6 +43,16 @@ public class MatrixSolver {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+	}
+	
+	private static void sum(String[] line) {
+		int sum = 0;
+		for (String i : line) {
+			sum += Integer.parseInt(i);
+		}
+
+		System.out.println(sum);
 	}
 	
 }
