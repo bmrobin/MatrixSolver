7a7a7a8eb870fd776b9019bda75788bc7a743f71
bmrobin <bmrobin@g.clemson.edu>
2014-03-24 15:49:13 -0400

    added code to read input test file

--- MatrixSolver/src/bmrobin/MatrixSolver.java
+++ MatrixSolver/src/bmrobin/MatrixSolver.java
@@ -1,5 +1,35 @@
 package bmrobin;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MatrixSolver {
 
+	public static void main(String[] args) {
+		loadData("testInput.txt");
+	}
+	
+	
+	private static void loadData(String filename) {
+		BufferedReader buffer = null;
+		String str = null;
+		try {
+			buffer = new BufferedReader(new FileReader(filename));
+			str = buffer.readLine();
+			while (str != null) {
+				System.out.println(str);
+				str = buffer.readLine();
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
